d6ab74246294def3d6a7550b7ca5e58d595075d6
mk0812 <matsuoka812930@gmail.com>
2019-10-22 18:37:36 +0900

    p5js add

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,91 @@
   text-align center
   color #2c3e50
   margin-top 60px
+
+
+#defaultCanvas0{
+  position: fixed; /*canvasを固定*/
+  top: 0; /*canvasを上に固定*/
+	z-index: -9999
+}
 </style>
+
+<script>
+class Module {
+  constructor(xOff, yOff, x, y, speed, unit) {
+    this.xOff = xOff;
+    this.yOff = yOff;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.unit = unit;
+    this.xDir = 1;
+    this.yDir = 1;
+  }
+
+  // Custom method for updating the variables
+  update() {
+    this.x = this.x + this.speed * this.xDir;
+    if (this.x >= this.unit || this.x <= 0) {
+      this.xDir *= -1;
+      this.x = this.x + 1 * this.xDir;
+      this.y = this.y + 1 * this.yDir;
+    }
+    if (this.y >= this.unit || this.y <= 0) {
+      this.yDir *= -1;
+      this.y = this.y + 1 * this.yDir;
+    }
+  }
+
+  // Custom method for drawing the object
+  draw(p) {
+    p.fill(255);
+    p.ellipse(this.xOff + this.x, this.yOff + this.y, 6, 6);
+  }
+}
+
+export default {
+	mounted() {   
+		const script = function (p5) {
+			let unit = 200;
+			let count;
+			let mods = [];
+
+			const width = window.innerWidth
+			const height = window.innerHeight
+
+			p5.setup = _ => {      
+				p5.createCanvas(width, height);
+				p5.noStroke();
+				let wideCount = width / unit;
+				let highCount = height / unit;
+				count = wideCount * highCount;
+
+				let index = 0;
+				for (let y = 0; y < highCount; y++) {
+					for (let x = 0; x < wideCount; x++) {
+						mods[index++] = new Module(
+							x * unit,
+							y * unit,
+							unit / 2,
+							unit / 2,
+							p5.random(0.05, 3.8),
+							unit
+						);
+					}
+				}
+			}     
+			// NOTE: Draw is here
+			p5.draw = _ => {      
+				p5.background(0);
+				for (let i = 0; i < count; i++) {
+					mods[i].update();
+					mods[i].draw(p5);
+				}
+			}  
+		}   
+		const P5 = require('p5');
+		new P5(script)
+	}
+}
+</script>
